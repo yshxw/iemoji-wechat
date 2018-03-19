8508941936335ef845c02a554a4ebc9df083312b
JianxinLi <justcoxier@gmail.com>
2018-03-19 10:58:37 +0800

    Add placeholder
    Change-Id: I59a89114d252a5fe406cc46bc66b48ec62aa3b4e

--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,50 @@
  * 当前正在编辑的句子
  */
 var index = 0;
-var placeHolders = ['第1句话', '第2句话', '第3句话', '第4句话', '第5句话', '第6句话', '第7句话', '第8句话', '第9句话'];
-var sentences = new Array();
+var placeholders = ['就算你是一流设计师', '就算你画图再牛逼', '我叫你改图你就得改图', '毕竟我是甲方', '甲方了不起啊','嗯~','sorry，甲方真的了不起', '以后叫他天天改图', '天天改图，天天改'];
+console.log(placeholders.length);
+var sentences = Array(placeholders.length).fill('')
+console.log(sentences.length);
 
 Page({
 
   previewImage: function (image) {
     wx.previewImage({
-      current: image, // 当前显示图片的http链接   
-      urls: this.data.imgalist // 需要预览的图片http链接列表   
+      current: image, // 当前显示图片的http链接
+      urls: this.data.imgalist // 需要预览的图片http链接列表
     })
-  },   
+  },
 
-  nextSentence: function(text) {
-    if (index == placeHolders.length - 1) {
+  nextSentence: function() {
+    if (index == placeholders.length - 1) {
       // 数据填写完毕
       this.make_gif()
     }else {
-      sentences[index] = text;
-      index++;      
+      sentences[index] = this.data.sentence;
+      console.log(this.data.sentence);
+      index++;
       this.setData({
-        textInfo: '第' + (index+1) + '句话',
-        preDisplay: 'visible'
+        textInfo: (index + 1) + ' / ' + placeholders.length,
+        preDisplay: 'visible',
+        sentence: '',
+        placeholder: placeholders[index],
       })
-      console.log(this.data.textInfo);
     }
   },
 
-  preSentence: function(text) {
+  preSentence: function() {
     if (index > 0) {
-      sentences[index] = text; 
-      index--;           
+      index--;
       this.setData({
-        textInfo: '第' + (index+1) + '句话',
+        textInfo: (index + 1) + ' / ' + placeholders.length,
+        sentence:sentences[index],
+        placeholder: placeholders[index],
       })
       if(index == 0) {
         this.setData({
-          preDisplay: 'hidden'
+          preDisplay: 'hidden',
         })
       }
-      console.log(this.data.textInfo);
     }
   },
 
@@ -50,23 +54,18 @@ Page({
    * 发送请求
    */
   make_gif: function() {
+    for(var i = 0; i < placeholders.length; i++) {
+      if(sentences[i] == '') {
+        sentences[i] = placeholders[i];
+      }
+    }
+    console.log(JSON.stringify(sentences));
     var that = this;
     wx.request({
       url: 'https://miniapp.codedragon.tech',
       data: {
         "font_size": "47",
-        "sentences": [
-          "sample sentence0",
-          "sample sentence1",
-          "sample sentence2",
-          "sample sentence3",
-          "sample sentence4",
-          "sample sentence5",
-          "sample sentence6",
-          "sample sentence7",
-          "sample sentence8",
-          "sample sentence9"
-        ]
+        "sentences": sentences
       },
       method: 'POST',
       success: function(res) {
@@ -80,70 +79,77 @@ Page({
     console.log('make gif')
   },
 
+  bindKeyInput: function (e) {
+    this.setData({
+      sentence: e.detail.value
+    })
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgalist: ['https://sorry.xuty.tk/cache/3cfab5c16f5148f88b72640c51cefde4.gif'],
-    textInfo: '第1句话',
-    ssentence: '',
-    preDisplay: 'hidden'
+    imgalist: ['https://t1.aixinxi.net/o_1c8u0i30i14c3c8c61mn5b6ksa.gif-w.jpg'],
+    textInfo: '1 / 9',
+    sentence: '',
+    placeholder:placeholders[0],
+    preDisplay: 'hidden',
+    input_focus: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
